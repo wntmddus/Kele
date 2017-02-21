d19c4062a6526078a52afe29ff63a2c7f7f34fba
Seung Yeon Joo <wntmddussla@hotmail.com>
2017-02-21 13:42:01 -0800

    assignment 3

--- lib/kele.rb
+++ lib/kele.rb
@@ -10,6 +10,17 @@ class Kele
     @auth_token = response["auth_token"]
   end
 
+  def get_me
+    response = self.class.get(base_api_endpoint("users/me"), headers: { "authorization" => @auth_token })
+    @user_data = JSON.parse(response.body)
+    @user_data.keys.each do |key|
+      self.class.send(:define_method, key.to_sym) do
+        @user_data[key]
+      end
+    end
+    @user_data
+  end
+
   private
 
   def base_api_endpoint(end_point)
